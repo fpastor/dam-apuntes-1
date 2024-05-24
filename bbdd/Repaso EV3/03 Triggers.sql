d662b0f9ebcec5a84e9374471ff400f81a8e4c72
Fernando Pastor <fpastor@icloud.com>
2024-05-24 13:52:09 +0200

    Update 03 Triggers.sql

--- bbdd/Repaso EV3/03 Triggers.sql
+++ bbdd/Repaso EV3/03 Triggers.sql
@@ -1,3 +1,30 @@
 -- -----------------------------------------------------------------------------
 --  PLANTILLA TRIGGER
 -- -----------------------------------------------------------------------------
+
+GO
+
+CREATE OR ALTER TRIGGER TX_TRIGGER ON TABLA
+INSTEAD OF INSERT 
+-- (AFTER , INSTEAD OF) <-> (INSERT , UPDATE , DELETE)
+AS
+
+BEGIN
+    BEGIN TRY
+
+        DECLARE @dato1 INT
+        DECLARE @dato2 INT
+
+        -- CÓDIGO DEL TRIGGER
+
+    END TRY
+
+    BEGIN CATCH
+        PRINT CONCAT('ERROR NUMBER: ', ERROR_NUMBER())
+        PRINT CONCAT('LINE: ', ERROR_LINE())
+        PRINT CONCAT('MESSAGE: ',  ERROR_MESSAGE())
+        PRINT CONCAT('PROCEDURE: ', ERROR_PROCEDURE())
+    END CATCH
+END
+
+GO
